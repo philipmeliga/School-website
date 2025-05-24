ebea23dbd5030d5c55fc3aee7d3565de85b7c19f
philipmeliga <philipmeliga01@gmail.com>
2025-05-24 17:08:36 +0100

    Add files via upload

--- dashboard.js
+++ dashboard.js
@@ -1,2 +1,20 @@
+function displayGreeting() {
+  const greetingElement = document.getElementById("greeting");
+  const now = new Date();
+  const hour = now.getHours();
+
+  if (hour < 12) {
+    greetingElement.textContent = "Good morning!";
+  } else if (hour < 17) {
+    greetingElement.textContent = "Good afternoon!";
+  } else {
+    greetingElement.textContent = "Good evening!";
+  }
+}
+
+// Call the function when the page loads
+window.onload = displayGreeting;
+
+
 const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
     document.getElementById('welcome-message').innerHTML = `Hello, ${loggedInUser.username}!`;
